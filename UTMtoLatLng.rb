ea0019f64b12a7fbfb953a339b25229912847433
Matthew Schwartz <schwartz.matthew@schwartzlink.net>
2011-01-07 00:19:06 -0600

    add formatting to UTMtoLatLng.rb

--- UTMtoLatLng.rb
+++ UTMtoLatLng.rb
@@ -1,6 +1,7 @@
 =begin
 
-Copyright (C) <year> by <copyright holders>
+Copyright (C) 2011 by Matthew Schwartz
+schwartz.matthew@schwartzlink.net
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
